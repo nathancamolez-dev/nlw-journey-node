b24f51958f0edcb57ff80784b9446e7b1799ce88
nathancamolez-dev <nathan.camolez@unesp.br>
2024-07-09 00:05:35 -0300

    feat: full route of create trip

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,5 +1,17 @@
 import nodemailer from 'nodemailer'
 
-async function getMailClient() {
-    
+export async function getMailClient() {
+    const account = await nodemailer.createTestAccount()
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: account.user,
+            pass: account.pass,
+        }
+    })
+
+    return transporter
 }
