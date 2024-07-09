c4e46fc65ae6b2f5ade49a05e0b00e8c9c7d1ea8
nathancamolez-dev <nathan.camolez@unesp.br>
2024-07-09 00:42:09 -0300

    feat: config eslint

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,85 +1,90 @@
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
-import { prisma } from "../lib/prisma";
-import dayjs from "dayjs";
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { prisma } from '../lib/prisma'
+import dayjs from 'dayjs'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 import 'dayjs/locale/pt-br'
-import { getMailClient } from "../lib/mail";
-import nodemailer from "nodemailer"
+import { getMailClient } from '../lib/mail'
+import nodemailer from 'nodemailer'
 
 dayjs.locale('pt-br')
 dayjs.extend(localizedFormat)
 
-export async function  createTrip(app: FastifyInstance){
-    app.withTypeProvider<ZodTypeProvider>().post('/trips',{
-        schema:{
-            body: z.object({
-                destination: z.string().min(4),
-                starts_at: z.coerce.date(),
-                ends_at: z.coerce.date(),
-                owner_name: z.string(),
-                owner_email: z.string().email(),
-                emails_to_invite: z.array(z.string().email())
-            })
-        }
-    }, async (request) => {
-        const { destination, starts_at, ends_at, owner_email, owner_name,emails_to_invite } = request.body
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/trips',
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
+        }),
+      },
+    },
+    async (request) => {
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_email,
+        owner_name,
+        emails_to_invite,
+      } = request.body
 
-        if(dayjs(starts_at).isBefore(new Date())) {
-            throw new Error('Start date must be in the future')
-                }
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error('Start date must be in the future')
+      }
 
-        if(dayjs(ends_at).isBefore(dayjs(starts_at))) {
-            throw new Error('End date must be after the start date')
-        }
+      if (dayjs(ends_at).isBefore(dayjs(starts_at))) {
+        throw new Error('End date must be after the start date')
+      }
 
-        
-
-        const trip = await prisma.trip.create({ 
-            data:
-            {
-                destination,
-                 starts_at,
-                  ends_at,
-                  participants:{
-                    createMany:{
-                        data: [
-                            {
-                        name: owner_name,
-                        email: owner_email,
-                        is_owner: true,
-                        is_confirmed: true
-                            },
-                            ...emails_to_invite.map(email => {
-                                return { email }
-                            })
-                        ]
-                    }
-                  }
-                }
-            })
-
-
-
-        const formattedStartDate = dayjs(starts_at).format('LL')
-        const formattedEndDate = dayjs(ends_at).format('LL')
-
-        const confirmationLink =  `http://localhost:3333/trips/${trip.id}/confirm`
-
-        const mail = await getMailClient()
-
-        const message = await mail.sendMail({
-            from:{
-                name: 'plann.er',
-                address: 'admin@plann.er.com'
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participants: {
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return { email }
+                }),
+              ],
             },
-            to: {
-                name: owner_name,
-                address: owner_email,
-            },
-            subject: `Confirme sua viagem para ${destination} em ${formattedStartDate}`,
-            html: `
+          },
+        },
+      })
+
+      const formattedStartDate = dayjs(starts_at).format('LL')
+      const formattedEndDate = dayjs(ends_at).format('LL')
+
+      const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`
+
+      const mail = await getMailClient()
+
+      const message = await mail.sendMail({
+        from: {
+          name: 'plann.er',
+          address: 'admin@plann.er.com',
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: `Confirme sua viagem para ${destination} em ${formattedStartDate}`,
+        html: `
             <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
             <p>Voce solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
             <p></p>
@@ -91,14 +96,14 @@ export async function  createTrip(app: FastifyInstance){
             <p></p>
             <p>Caso você não saiba do que se trata esse e-mail, apenas ignore.</p>
             </div>
-            `.trim()
-    })
+            `.trim(),
+      })
 
-    console.log(nodemailer.getTestMessageUrl(message))
+      console.log(nodemailer.getTestMessageUrl(message))
 
-        return {
-            tripId: trip.id
-        }
-    })
-
+      return {
+        tripId: trip.id,
+      }
+    },
+  )
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import fastify from "fastify";
-import cors from "@fastify/cors";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { createTrip } from "./routes/create-trip";
-import { confirmTrip } from "./routes/confirm-trip";
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createTrip } from './routes/create-trip'
+import { confirmTrip } from './routes/confirm-trip'
 
 const app = fastify()
 
-app.register(cors,{
-    origin: '*'
+app.register(cors, {
+  origin: '*',
 })
 
 app.setValidatorCompiler(validatorCompiler)
@@ -16,7 +19,6 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createTrip)
 app.register(confirmTrip)
 
-
-app.listen({ port: 3333}).then(() => {
-    console.log("Server is listening on port 3333");
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is listening on port 3333')
 })
